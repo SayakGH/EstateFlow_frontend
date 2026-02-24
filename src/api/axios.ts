0d46259bcac0a2cfa94c70751898f7fa8694fc9c
sayakghosh <sayakghosh2004sg@gmail.com>
2026-02-24 22:18:22 +0530

    url changes

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -3,8 +3,8 @@ import axios from "axios";
 const api = axios.create({
   baseURL:
     import.meta.env.VITE_API_URL ||
-    //"https://filbk4rekzubnwfov74tvvfwde0rshvr.lambda-url.ap-south-1.on.aws/api/v1" ||
-    "http://localhost:8080/api/v1",
+    "https://filbk4rekzubnwfov74tvvfwde0rshvr.lambda-url.ap-south-1.on.aws/api/v1",
+  // "http://localhost:8080/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
